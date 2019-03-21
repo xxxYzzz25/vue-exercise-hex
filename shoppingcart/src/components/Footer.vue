e8a35703df640f5204083d6f390dba3a4b9411f5
Your Name <xxxzz925@gmail.com>
2019-03-21 17:44:18 +0800

    0321

---
+++ shoppingcart/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+<footer class="footer py-3 mt-5">
+    <div class="container">
+      <ul class="list-inline text-center contact-info">
+        <li class="list-inline-item">
+          <a class="text-primary" href="#">
+            <i class="fab fa-facebook-f"></i></a>
+        </li>
+        <li class="list-inline-item">
+          <a class="text-primary" href="#">
+            <i class="fab fa-github"></i></a>
+        </li>
+        <li class="list-inline-item">
+          <a class="text-primary" href="#">
+            <i class="fab fa-instagram"></i></a>
+        </li>
+         <li class="list-inline-item">
+          <a class="text-primary" href="mailto:qvod100203006@gmail.com"  type="mail">
+            <i class="fas fa-envelope"></i></a>
+        </li>
+      </ul>
+      <p class="text-center copyright">©Copright 2018   <a  href="mailto:qvod100203006@gmail.com"  type="mail">Chao-YuPing</a></p>
+    </div>
+  </footer>
+</template>
---
+++ shoppingcart/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- headers -->
+  <nav class="navbar navbar-light topNavbar mb-4">
+    <div class="container">
+      <div class="d-inline-flex topNavbar-link">
+        <router-link to="/" class="icon-nav icon-home">
+          <div class="icon-text">首&emsp;頁</div>
+        </router-link>
+        <router-link to="/discount" class="icon-nav icon-discount">
+          <div class="icon-text">優惠活動</div>
+        </router-link>
+      </div>
+
+      <router-link class="navbar-brand logo" to="/">
+        <img src="../assets/images/logo.png" class="img-fluid" alt>
+      </router-link>
+
+      <div class="d-inline-flex align-items-center topNavbar-link">
+        <router-link to="/admin" class="icon-nav icon-admin">
+          <div class="icon-text">管理商品</div>
+        </router-link>
+        <router-link to="/login" class="icon-nav icon-member">
+          <div class="icon-text">登&emsp;入</div>
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
